24e84f21dd29ef63207e08bbd5948abb591a7479
Rebekah Cha <rcha@mit.edu>
2013-11-30 14:42:19 -0500

    rectangle finally draws in all 4 quadrants. added some comments

--- src/tools/RectangleController.java
+++ src/tools/RectangleController.java
@@ -4,6 +4,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D;
 
@@ -40,10 +41,12 @@ public class RectangleController implements ToolController {
         Graphics2D g2 = (Graphics2D) drawingBuffer.getGraphics();
 
         // Get points
-        int x = Integer.valueOf(args[1]);
-        int y = Integer.valueOf(args[2]);
-        int width = Integer.valueOf(args[3]) - x;
-        int height = Integer.valueOf(args[4]) - y;
+        int x1 = Integer.valueOf(args[1]); //lastX
+        int y1 = Integer.valueOf(args[2]); //lastY
+        int x2 = Integer.valueOf(args[3]); //currentX
+        int y2 = Integer.valueOf(args[4]); //currentY
+        int width = Math.abs(x2-x1);
+        int height = Math.abs(y2-y1);
         
         //Define brushColor
         int colorInt = Integer.valueOf(args[5]);
@@ -57,62 +60,54 @@ public class RectangleController implements ToolController {
         // Draw
         g2.setColor(new Color(colorInt));
         g2.setStroke(new BasicStroke(brush));
-        g2.drawRect(x, y, width, height);
+        //rectangle is anchored at the least x and y values (top left corner)
+        g2.drawRect(Math.min(x1,x2), Math.min(y1,y2), width, height);
 
-        // IMPORTANT! every time we draw on the internal drawing buffer, we
-        // have to notify Swing to repaint this component on the screen.
         canvas.repaint();
     }
 
     /*
      * When mouse button is pressed down, start drawing.
+     * Sets the initial point from which the rectangle will be drawn from
      */
     public void mousePressed(MouseEvent e) {
-        lastX = e.getX();
-        lastY = e.getY();
-
-        // Get brush color
         brushColor = canvas.getBrushColor();
-
-
-        // Get brushSize
-
         brushSize = canvas.getBrushSize();
 
-        // Get brush color
-//        color = canvas.getFillColor();
-
-        // Check if it has fill
-        if (canvas.hasFill()) {
-            hasFill = 1;
-        } else {
-            hasFill = 0;
-        }
-
-        rectangle.setFrame(lastX, lastY, 0, 0);
-        canvas.setSurfaceShape(rectangle);
+    	lastX = e.getX(); lastY = e.getY();
+    	rectangle.setFrame(lastX, lastY, 0, 0);
+    	updateSize(e);
     }
 
+    /*
+     * Sends information to the server and resets the canvas surface shape from 'rectangle' to 'null'
+     */
     public void mouseReleased(MouseEvent e) {
-        // Get position
-        int x = e.getX();
-        int y = e.getY();
-        
-        // Sends info to the server
-        canvas.mClient.send("drawrect " + lastX + " " + lastY + " " + x + " "
-                + y + " " + brushColor + " "
+        canvas.mClient.send("drawrect " + lastX + " " + lastY + " " + e.getX() + " "
+                + e.getY() + " " + brushColor + " "
                 + brushSize + " " + hasFill + " " + fillColor);
         canvas.setSurfaceShape(null);
     }
 
+    /*
+     * Updates the current rectangle as the mouse is being dragged
+     */
     public void mouseDragged(MouseEvent e) {
-        int x = e.getX();
-        int y = e.getY();
-
-        int width = x - lastX;
-        int height = y - lastY;
-        rectangle.setFrame(lastX, lastY, width, height);
-        canvas.repaint();
+        updateSize(e);
+    }
+    
+    /*
+     * Updates the current rectangle
+     */
+    private void updateSize(MouseEvent e){
+    	int x = e.getX();
+    	int y = e.getY();
+    	
+    	int width = (int) Math.abs(x - lastX);
+    	int height = (int) Math.abs(y - lastY);
+    	rectangle.setFrame(Math.min(x, lastX), Math.min(y, lastY), width, height);
+    	canvas.setSurfaceShape(rectangle);
+    	canvas.repaint();
     }
 
     // Ignore all these other mouse events.
