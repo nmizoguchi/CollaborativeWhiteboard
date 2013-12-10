4a88444411c6b3edd0a2caf6c2ccfd8e7ca71aa7
nickmm <nickmm@mit.edu>
2013-12-10 09:50:24 -0500

    Setting look and feel to windows Vista

--- src/client/Main.java
+++ src/client/Main.java
@@ -4,39 +4,59 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import client.gui.WhiteboardGUI;
 
 public class Main {
-    /**
-     * Main program. Make a window containing a Canvas.
-     */
-    public static void main(String[] args) throws UnknownHostException,
-            IOException {
+	/**
+	 * Main program. Make a window containing a Canvas.
+	 */
+	public static void main(String[] args) throws UnknownHostException,
+			IOException {
 
-        // Request for the server IP
-        // TODO: Check for invalid IP's and problems with the connection!
-        String server = JOptionPane.showInputDialog("Server IP:");
-        final ClientApplication client = new ClientApplication(server, 4444);
+		// Request for the server IP
+		// TODO: Check for invalid IP's and problems with the connection!
+		String server = JOptionPane.showInputDialog("Server IP:");
+		final ClientApplication client = new ClientApplication(server, 4444);
 
-        // Username dialog will continue to pop-up if the user clicks cancel
-        String username;
-        do {
-            username = JOptionPane.showInputDialog("Username:");
-        } while (username == null);
-        if (username.equals("")){
-        	username = "Anonymous" + (int)Math.floor(Math.random()*10000);
-        }
+		// Username dialog will continue to pop-up if the user clicks cancel
+		String username;
+		do {
+			username = JOptionPane.showInputDialog("Username:");
+		} while (username == null);
+		if (username.equals("")) {
+			username = "Anonymous" + (int) Math.floor(Math.random() * 10000);
+		}
 
-        WhiteboardGUI GUI = new WhiteboardGUI(client);
-        
-        // Important, otherwise don't create the image buffer.
-        GUI.setVisible(true);
-        
-        // Initialize the client. To initialize a client, we need a username.
-        client.initialize(GUI, username);
-        
-        GUI.setVisible(true);
-        
-    }
+		/*
+		 * Set the look and feel to the Windows Vista layout, so everyone sees
+		 * the same layout
+		 */
+		try {
+			// Set cross-platform Java L&F (also called "Metal")
+			UIManager.setLookAndFeel(UIManager
+					.getCrossPlatformLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		} catch (ClassNotFoundException e) {
+			// handle exception
+		} catch (InstantiationException e) {
+			// handle exception
+		} catch (IllegalAccessException e) {
+			// handle exception
+		}
+		
+		WhiteboardGUI GUI = new WhiteboardGUI(client);
+
+		// Important, otherwise don't create the image buffer.
+		GUI.setVisible(true);
+
+		// Initialize the client. To initialize a client, we need a username.
+		client.initialize(GUI, username);
+
+		GUI.setVisible(true);
+
+	}
 }
